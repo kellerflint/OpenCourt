2429a3317937a24946d0d9f4265a12378dab79fc
LibanBritt <liban3367@gmail.com>
2025-10-02 11:30:43 -0700

    model for user is done!

--- api/model/user.js
+++ api/model/user.js
@@ -3,7 +3,26 @@ import { DataTypes, FLOAT } from "sequelize";
 
 const {STRING, INTEGER, BOOLEAN, TEXT, JSON } = DataTypes;
 
-const User = sequelize.define('user')
+const User = sequelize.define('user', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
 
 const Event = sequelize.define('event')
 
