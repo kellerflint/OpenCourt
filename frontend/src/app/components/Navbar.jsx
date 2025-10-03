f2ef55cb6c4d1736c140f102f8870dcfa4ae1a8d
Stefanurkal <stephenfranada@gmail.com>
2025-10-03 09:12:07 -0700

    issue with pushing front end, fixed errors pushing again on a  branch for the frontend named frontend

---
+++ frontend/src/app/components/Navbar.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import Image from 'next/image';
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
+
+export default function Navbar() {
+  return (
+    <AppBar position="static">
+
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ display: "flex", alignItems: "left", gap: 1 }}>
+          <Image src="/logo.png" alt="Logo" width={75} height={50} />
+        </Box>
+        <Button color="inherit" component={Link} href="/">
+          Home
+        </Button>
+        <Button color="inherit" component={Link} href="/request">
+          Find Open Courts
+        </Button>
+         <Button color="inherit" component={Link} href="/new">
+          Submit A New Game
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
---
+++ frontend/src/app/layout.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar.jsx"; 
+import "./globals.css";
+
+export const metadata = {
+  title: "Open Court",
+  description: "Find Open Pick Up Games",
+  icons: {
+    icon: "/favicon.ico", 
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {}
+      <body>
+        <Navbar /> 
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
