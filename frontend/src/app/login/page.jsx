bfb67f3ace8506ed6d4c5725b0f785e0a639e8a0
LibanBritt <liban3367@gmail.com>
2025-10-07 11:32:15 -0700

    working on user login. Made folder and made the first box and input field

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,2 +1,27 @@
-import Navbar from "./components/Navbar.jsx"; 
-import "./globals.css";
+import React from "react";
+import { FormControl, InputLabel, Input, Button, TextField, Box } from "@mui/material";
+
+//setting up effects
+
+
+//functions
+
+
+export default function LoginPage() {
+  return (
+    <Box
+      componet="form"
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+        required
+        id="outlined-required"
+        label="Required"
+        defaultValue="Hello World"
+        />
+      </div>
+    </Box>
+  );
+}
