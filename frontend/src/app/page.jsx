aa9ed2f66cbe6c4bfef51a56c000074b5da7f943
brittLiban <britt.liban@student.greenriver.edu>
2025-10-09 11:37:38 -0700

    I figured out to how get rid of autocomplete at the browser level... I can now cry in peace!

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -4,6 +4,27 @@ import {Box, TextField, Button, Typography } from '@mui/material'
 
 export default function LoginPage() {
   return (
+    <Box component="main" sx={{ p: 3, maxWidth: 420, mx: "auto"}}>
+      <Typography variant="h5" gutterBottom>Login</Typography>
 
+      <Box component="form" noValidate autoComplete='off'>
+        <TextField
+        label="Email"
+        type="email"
+        fullWidth
+        margin="normal"
+        autoComplete="off"
+        inputProps={{ autoComplete: "off" }}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          fullWidth
+          margin="normal"
+          inputProps={{ autoComplete: "new-password" }}
+        />
+        <Button type='submit' variant='contained' fullWidth sx={{ mt:2}}>Submit</Button>
+      </Box>
+    </Box>
   );
 }
