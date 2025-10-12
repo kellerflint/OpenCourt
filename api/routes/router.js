1ca8d3eac91f53d2d312ff4c38824b355b68e6e1
Stefanurkal <stephenfranada@gmail.com>
2025-10-12 11:23:32 -0700

    update of my work for partner to see,model crud & controller

--- api/routes/router.js
+++ api/routes/router.js
@@ -3,9 +3,20 @@ import db from '../db/db.js';
 
 const router = Router();
 
-router.get('/', (_req, res) => {
+router.get('/', (req, res) => {
   res.json({ ok: true, name: 'OpenCourt API' });
 });
+
+router.get('/new', (_req, res) => {
+
+    res.render('new_entry_form', { title: 'New Submission Form' });
+});
+
+router.get('/products/all', getAllcourts);
+
+router.post('/post', (req, res) => {
+  
+})
 
 
 
