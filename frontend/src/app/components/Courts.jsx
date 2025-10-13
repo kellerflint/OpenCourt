c132867d2d38e869e4ff63b12809c4f741911f2d
Stefanurkal <stephenfranada@gmail.com>
2025-10-12 23:18:54 -0700

    added next js pages for planned routes

---
+++ frontend/src/app/components/Courts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import RentalCard from "./CourtsCard.jsx";
+import { Grid } from "@mui/material";
+
+// will have all the court information from the lower level component courtsCard
+
+const courts = 
