194c5f297d7ce0a6fe3dd2a6aaa9226fbc85b04d
brittLiban <britt.liban@student.greenriver.edu>
2025-10-13 15:36:41 -0700

    Implement session management with Sequelize store and initialize passport middleware

--- api/server.js
+++ api/server.js
@@ -1,23 +1,60 @@
 import express from 'express';
 import cors from 'cors';
-import db, { initDB } from './db/db.js';            
+import  { initDB, sequelize } from './db/db.js';            
 import routes from './routes/router.js';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import connectSession from "connect-session-sequelize";
-import sequelize from "./db/db.js";
+import passport from './services/passport.js'         
+
 
 const app = express();
 const PORT = 3001;
 
 //allow other urls to access this
-app.use(cors());
+app.use(cors({
+  origin: true,        
+  credentials: true,   
+}));
 app.use(express.json());
+app.use(cookieParser())
+
+
+
+//sessions
+// create Sequelize-backed store for express-session
+const SequelizeStore = connectSession(session.Store); 
+// uses existing sequelize instance
+const store = new SequelizeStore({db : sequelize})
+// create sessions table if missing
+await store.sync();
+
+
+app.use(
+  session({
+    secret: "b3f9a6d2e7c1f48a9d2b4c7f3e1a5c6d8f2b9a0c7e4d1f3b6a8c9e0d1f2b3a4",   // need to change lol,  used to sign the session ID cookie (prevents tampering)
+    resave: false,                     // don't re-save session if nothing changed
+    saveUninitialized: false,          // don't create empty sessions before login
+    store,                             // where sessions are actually stored (DB)
+    cookie: {
+      sameSite: "lax",                 // prevents most cross-site issues
+      secure: false,                   // cookie can be sent over HTTP (not just HTTPS)
+      maxAge: 1000 * 60 * 60 * 24,     // session expires after 1 day
+    },
+  })
+);
+
+
+// middleware verifying user is logged in on each route
+app.use(passport.initialize());
+app.use(passport.session());
+
+await initDB()
+
+
 
 //server mounting
 app.use('/api', routes);
-
-initDB()
 
 app.listen(PORT, () => {
   console.log(`API listening on http://localhost:${PORT}/api`);
